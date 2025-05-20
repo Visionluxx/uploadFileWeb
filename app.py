f17e481a38878e6f616b0cfba5c88a135d292aac
Visionluxx <honhatquang247@gmail.com>
2025-05-21 00:07:13 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, render_template, request, session, send_from_directory, render_template_string
+
+
+app = Flask(__name__, template_folder="template")
+
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024
+
+@app.route('/', methods=["POST", "GET"])
+def upload():
+    if request.method=="POST":
+      file = request.files['uploadedFile']
+      if file and file.filename:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+        print ("saved to"+filepath)
+    return render_template("UploadFile.html")
+
+@app.route ('/uploaded')
+def uploaded():
+    folder=os.listdir(app.config["UPLOAD_FOLDER"])
+    fileList=[f for f in folder if os.path.isfile(os.path.join(app.config["UPLOAD_FOLDER"],f))]
+    return render_template_string("""<h1> there is {{num}} files
+    <ul>
+    {%for file in folder %}
+      <li>{{file}} - 
+        <a href="{{url_for('findFile', filename=file)}}">download</a>
+    {%endfor%}
+    </ul>
+    <h1>please go to /download/__filename__ to download file
+    """, folder=fileList, num=len(fileList))
+
+@app.route("/download/<filename>")
+def findFile(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
